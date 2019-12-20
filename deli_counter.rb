e612572f1705b187b680fac693117452a66e4d69
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-20 01:38:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def deli_counter (array)
+  if array== []
+  puts ("the line is empty")
+else
+  puts ("there are poeple in line ")
+end
+
+def deli_counter (array)
+  array.push ("Ada")
+end
+def deli_counter (array)
+  array.push ("Grace")
+end
