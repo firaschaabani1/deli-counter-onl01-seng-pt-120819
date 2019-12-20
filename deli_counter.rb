120a79ec190c92a337a3cd38898a52388767a06b
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-20 01:46:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 # Write your code here.
-def deli_counter (array)
+def line (array)
   if array== []
   puts ("the line is empty")
 else
   puts ("there are poeple in line ")
 end
 
-def deli_counter (array)
-  array.push ("Ada")
+def take_a_number (array)
+if array==[]
+  puts "there is nobody in line"
+else
+  puts "there are alredy poeple in line"
 end
-def deli_counter (array)
-  array.push ("Grace")
-end
+  
